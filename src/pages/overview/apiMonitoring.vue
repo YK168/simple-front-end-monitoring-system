da228c39e34f01cf1495dd462cc45a405d93fdb5
YK <1379327521@qq.com>
2022-08-15 20:56:42 +0800

    feat: 项目目录调整

---
+++ src/pages/overview/apiMonitoring.vue
@@ -0,0 +1,13 @@
+<template>
+<div>这里是网络请求监控</div>
+</template>
+
+<script>
+export default {
+  name: 'apiMonitoring'
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/pages/overview/pageDetail.vue
@@ -0,0 +1,13 @@
+<template>
+<div>这里是页面报错详情</div>
+</template>
+
+<script>
+export default {
+  name: 'pageDetail'
+}
+</script>
+
+<style scoped>
+
+</style>
